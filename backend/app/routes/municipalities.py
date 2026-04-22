824e35fee6ef7de44b634257f7f7b2b118933be1
Kaduba Egor <egor.kaduba@mail.ru>
2026-04-22 18:40:58 +0300

    Добавил ручки для поиска муниципалитета по названию

--- backend/app/routes/municipalities.py
+++ backend/app/routes/municipalities.py
@@ -13,9 +13,19 @@ mun_router = APIRouter(
 def read_municipalities(
         session: SessionDep,
         offset: int = 0,
-        limit: int = Query(default=100, le=100)
+        limit: int = Query(default=100, le=500)
 ) -> list[Municipality]:
     municipalities = list(session.exec(select(Municipality).offset(offset).limit(limit)).all())
     if not municipalities:
         raise HTTPException(status_code=404, detail="Муниципалитеты не найдены")
     return municipalities
+
+@mun_router.get("/{municipality_name}", summary="Поиск муниципалитета по названию")
+def read_municipality(
+    municipality_name: str,
+    session: SessionDep
+) -> Municipality:
+    municipality = session.exec(select(Municipality).where(Municipality.municipality_name == municipality_name)).first()
+    if not municipality:
+        raise HTTPException(status_code=404, detail=f"Муниципалитет с названием [{municipality_name}] не найден")
+    return municipality
