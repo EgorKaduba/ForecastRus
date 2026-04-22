02d0134fb550cf68a23c624279fb60833cbb9a9f
Kaduba Egor <egor.kaduba@mail.ru>
2026-04-22 11:21:44 +0300

    Получение региона по названию

--- backend/app/routes/regions.py
+++ backend/app/routes/regions.py
@@ -1,3 +1,5 @@
+from multiprocessing.managers import rebuild_as_list
+
 from fastapi import APIRouter, Query, HTTPException
 from sqlmodel import select
 
@@ -14,9 +16,20 @@ reg_router = APIRouter(
 def read_regions(
         session: SessionDep,
         offset: int = 0,
-        limit:int = Query(default=100, le=100)
+        limit: int = Query(default=100, le=100)
 ) -> list[Region]:
     regions = list(session.exec(select(Region).offset(offset).limit(limit)).all())
     if regions:
         return regions
     raise HTTPException(status_code=404, detail="Субъекты не найдены")
+
+
+@reg_router.get("/{region_name}")
+def read_region(
+        region_name: str,
+        session: SessionDep
+) -> Region:
+    region = session.exec(select(Region).where(Region.region_name == region_name)).first()
+    if not region:
+        raise HTTPException(status_code=404, detail="Регион не найден")
+    return region
