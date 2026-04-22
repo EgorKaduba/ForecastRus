8eee2f63529b94c149b3e4529670404940a02571
Kaduba Egor <egor.kaduba@mail.ru>
2026-04-22 17:38:39 +0300

    Добавлена новая ручка. Получение информации о регионе по id и году

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,3 +7,24 @@ class Region(SQLModel, table=True):
 
     region_id: int | None = Field(default=None, primary_key=True)
     region_name: str = Field(index=True)
+
+
+class RegionAggregated(SQLModel, table=True):
+    __tablename__ = "region_aggregated"
+    __table_args__ = {"schema": "demography"}
+
+    region_agg_id: int | None = Field(default=None, primary_key=True)
+    region_id: int | None = Field(default=None, foreign_key="regions.region_id")
+    year: int
+    total_population: int = Field(default=0)
+    total_births: int = Field(default=0)
+    total_deaths: int = Field(default=0)
+    total_migration: int = Field(default=0)
+    avg_birth_rate: float = Field(default=0.0, max_digits=10, decimal_places=3)
+    avg_mortality_rate: float = Field(default=0.0, max_digits=10, decimal_places=3)
+    avg_migration_rate: float = Field(default=0.0, max_digits=10, decimal_places=3)
+    municipalities_count: int = Field(default=0)
+
+
+class RegionDataWithName(RegionAggregated):
+    region_name: str | None = Field(default=None)
--- backend/app/routes/regions.py
+++ backend/app/routes/regions.py
@@ -1,10 +1,8 @@
-from multiprocessing.managers import rebuild_as_list
-
 from fastapi import APIRouter, Query, HTTPException
 from sqlmodel import select
 
 from ..deps import SessionDep
-from ..models import Region
+from ..models import Region, RegionAggregated, RegionDataWithName
 
 reg_router = APIRouter(
     prefix="/regions",
@@ -21,7 +19,7 @@ def read_regions(
     regions = list(session.exec(select(Region).offset(offset).limit(limit)).all())
     if regions:
         return regions
-    raise HTTPException(status_code=404, detail="Субъекты не найдены")
+    raise HTTPException(status_code=404, detail="Субъекты РФ не найдены")
 
 
 @reg_router.get("/{region_name}")
@@ -31,5 +29,21 @@ def read_region(
 ) -> Region:
     region = session.exec(select(Region).where(Region.region_name == region_name)).first()
     if not region:
-        raise HTTPException(status_code=404, detail="Регион не найден")
+        raise HTTPException(status_code=404, detail=f'Субъект РФ с названием "{region_name}" не найден')
     return region
+
+
+@reg_router.get("/{region_id}/{year}")
+def get_region_data(
+        region_id: int,
+        year: int,
+        session: SessionDep
+) -> RegionDataWithName:
+    statement = select(RegionAggregated).where(RegionAggregated.region_id == region_id, RegionAggregated.year == year)
+    region_data = session.exec(statement).first()
+    region_name = session.get(Region, region_id)
+    data_dict = region_data.dict()
+    data_dict["region_name"] = region_name.region_name
+    if not region_data:
+        raise HTTPException(status_code=404, detail=f'Информация о субъекте с id={region_id} за {year} год не найдена')
+    return RegionDataWithName(**data_dict)
