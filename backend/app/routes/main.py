b7a5bc2d530fa0b94ec8d1775cc9a274957a5e2e
LabManOO1 <maxmaximmax37@gmail.com>
2026-04-25 22:46:42 +0300

    Создал роутер и ручку для генерации отчета

--- backend/app/routes/main.py
+++ backend/app/routes/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from . import regions, municipalities, heatmap
+from . import regions, municipalities, heatmap, report
 
 api_router = APIRouter()
 api_router.include_router(regions.reg_router)
 api_router.include_router(municipalities.mun_router)
 api_router.include_router(heatmap.heatmap_router)
+api_router.include_router(report.rep_router)
---
+++ backend/app/routes/report.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Query, HTTPException
+from sqlmodel import select
+import os
+
+from ..tools.creating_report import get_population_by_region, get_municipalities_by_id, create_report
+from ..deps import SessionDep
+
+rep_router = APIRouter(
+    prefix="/report",
+    tags=["Отчет"]
+)
+
+@rep_router.get("/", summary="Сгенерировать отчет")
+def generate_report(
+        session: SessionDep,
+        id: int = Query(..., description="ID региона или муниципалитета"),
+        type: str = Query(..., description="Тип: 'region' или 'municipality'"),
+        year_from: int = Query(..., ge=2010, le=2030),
+        year_to: int = Query(..., ge=2010, le=2030)
+) -> str:
+    try:
+        if type == "region":
+            data = get_population_by_region(session, id, year_from, year_to)
+        elif type == "municipality":
+            data = get_municipalities_by_id(session, id, year_from, year_to)
+        else:
+            raise HTTPException(status_code=400, detail="Неверный тип. Используйте 'region' или 'municipality'")
+
+        if not data:
+            raise HTTPException(status_code=404, detail="Нет данных для указанного периода")
+
+        file_name = create_report(id, type, year_from, year_to)
+
+        path = file_name
+        if os.path.exists(path):
+            return path
+        else:
+            raise HTTPException(status_code=500, detail="Ошибка при генерации отчёта")
+
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ошибка: {str(e)}")
