6f8d90f9e90786c9408167379a692f53b9150c32
Kaduba Egor <egor.kaduba@mail.ru>
2026-04-27 01:46:53 +0300

    Добавление карты

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 
 import Header from "./components/Header/Header.jsx";
 import Headline from "./components/Headline/Headline.jsx";
+import Map from "./components/Map/Map.jsx";
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Headline/>
+            <Map/>
         </div>
     )
 }
