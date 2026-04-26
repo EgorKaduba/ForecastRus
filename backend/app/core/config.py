d768154443cdb3d9985c6a0bef460a73033d8345
Kaduba Egor <egor.kaduba@mail.ru>
2026-04-27 00:27:35 +0300

    Добавление CORS

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import PostgresDsn, computed_field
+from pydantic import PostgresDsn, computed_field, AnyUrl, BeforeValidator
+from typing import Annotated, Any
 
+
+def parse_cors(v: Any) -> list[str] | str:
+    if isinstance(v, str) and not v.startswith("["):
+        return [i.strip() for i in v.split(",") if i.strip()]
+    elif isinstance(v, list | str):
+        return v
+    raise ValueError(v)
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -10,12 +18,24 @@ class Settings(BaseSettings):
     )
     PROJECT_NAME: str
     PROJECT_VERSION: str = "1.0.0"
+    FRONTEND_HOST: str = "http://localhost:5173"
     ADMIN_NAME: str
     ADMIN_EMAIL: str
 
     DEBUG: bool = True
 
     API_V1_PREFIX: str
+
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def all_cors_origins(self) -> list[str]:
+        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
+            self.FRONTEND_HOST
+        ]
 
     POSTGRES_SERVER: str
     POSTGRES_PORT: int = 5432
